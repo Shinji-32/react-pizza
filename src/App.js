7de24bce67d08edb395306388c3113c2bb387ea4
Julia Lehoida <JuliyaScarlet83@gmail.com>
2025-04-16 19:12:55 +0300

    useState, reactDOM, probs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import BlockPizza from "./components/BlockPizza";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories/>
+            <Sort/>
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <BlockPizza title="Чизбургер-пицца" price={500}/>
+            <BlockPizza title="Мексиканская" price={350}/>
+            <BlockPizza title="Гавайская" price={400}/>
+            <BlockPizza title="4 Мозераті" price={200}/>
+            <BlockPizza title="4 Мозераті" price={520}/>
+            <BlockPizza title="Пепперони" price={230}/>
+            <BlockPizza title="Пепперони" price={230}/>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
