08fa2ca18b9f74cc8c56b61cc3b89ae43b2e2162
Julia Lehoida <JuliyaScarlet83@gmail.com>
2025-04-22 16:18:40 +0300

    spread-operator

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import BlockPizza from "./components/BlockPizza";
 
+import pizzas from "./assets/pizzas.json"
+
+
+console.log(pizzas);
 
 function App() {
   return (
@@ -18,14 +22,12 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <BlockPizza title="Чизбургер-пицца" price={500}/>
-            <BlockPizza title="Мексиканская" price={350}/>
-            <BlockPizza title="Гавайская" price={400}/>
-            <BlockPizza title="4 Мозераті" price={200}/>
-            <BlockPizza title="4 Мозераті" price={520}/>
-            <BlockPizza title="Пепперони" price={230}/>
-            <BlockPizza title="Пепперони" price={230}/>
-            
+          <div className="content__items">
+            {pizzas.map((obj) => (
+                <BlockPizza {...obj} />
+              ))
+            }
+          </div>
           </div>
         </div>
       </div>
